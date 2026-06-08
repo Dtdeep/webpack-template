73eff225e7671378a2da4c1d43d2fd2f33cce03b
Dtdeep <dumagandesthiny@gmail.com>
2026-06-08 13:20:24 +0800

    Add Prettier package

--- webpack.common.js
+++ webpack.common.js
@@ -1,25 +1,25 @@
- import path from 'node:path';
- import { fileURLToPath } from 'node:url';
- import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
- const __filename = fileURLToPath(import.meta.url);
- const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
- export default {
-   entry: {
-     app: './src/index.js',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       template: './src/index.html',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-   module: {
+export default {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
@@ -32,8 +32,7 @@
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      }
-      
+      },
     ],
   },
- };
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
- import { merge } from 'webpack-merge';
- import common from './webpack.common.js';
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
- export default merge(common, {
-   mode: 'development',
-   devtool: 'eval-source-map',
-   devServer: {
-    static: './dist',
-    watchFiles: ['./src/index.html'],
-  }
- });
+export default merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["./src/index.html"],
+  },
+});
